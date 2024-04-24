04ec0efd54a8b27c792a42eb7772ea0449b0a122
Lucas Simidu <lucas.simidu@outlook.com>
2024-04-24 11:17:47 -0300

    att

---
+++ Exercícios de Fixação/index.js
@@ -0,0 +1,10 @@
+function clicou(){
+    alert('Voce clicou msm q otário kkkkkkkkkkkkkkkkkkkk')
+}
+
+function variavel(){
+    let nome = window.prompt('Qual o seu nome?:')
+    alert(`Olá, ${nome}! É um prazer te conhecer!`)
+    document.getElementById("conteudo").innerHTML = `<label>O seu nome é ${nome}</label>`
+}
+
